47f69b16dd5e62099b430135e8f4b8c14d6b7ac6
Guillermo Riaño <guilledark17@gmail.com>
2024-04-26 20:09:03 -0600

    ✅ Adding Tests Script: Sword

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -6,9 +6,13 @@ public class Sword : MonoBehaviour
 {
     private PlayerControls playerControls;
     private Animator myAnimator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
 
     private void Awake()
     {
+        playerController = GetComponent<PlayerController>();
+        activeWeapon = GetComponent<ActiveWeapon>();
         myAnimator = GetComponent<Animator>();
         playerControls = new PlayerControls();
     }
@@ -18,13 +22,35 @@ public class Sword : MonoBehaviour
         playerControls.Enable();
     }
 
-    private void Start()
+    void Start()
     {
         playerControls.Combat.Attack.started += _ => Attack();
+    }
+
+    private void Update()
+    {
+        MouseFollowWithOffset();
     }
 
     private void Attack()
     {
         myAnimator.SetTrigger("Attack");
+    }
+
+    private void MouseFollowWithOffset()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+        }
+        else
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
     }
 }
