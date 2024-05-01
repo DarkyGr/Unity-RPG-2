f534379e4f20c1062db37ed9dc8c8978a4fd5827
Guillermo Riaño <guilledark17@gmail.com>
2024-05-01 07:46:53 -0600

    ✅ Adding Script: Damage

---
+++ Assets/Scripts/DamageSource.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageSource : MonoBehaviour
+{
+    [SerializeField] private int damageAmount = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<EnemyHealth>())
+        {
+            EnemyHealth enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+            enemyHealth.TakeDamage(damageAmount);
+        }
+    }
+}
