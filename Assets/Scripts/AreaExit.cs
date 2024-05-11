7f95014307fe28d71767656f528ef514d1d5da53
Guillermo Riaño <guilledark17@gmail.com>
2024-05-11 17:17:24 -0600

    ✅ Adding Scripts: Change of Scene

---
+++ Assets/Scripts/AreaExit.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AreaExit : MonoBehaviour
+{
+    [SerializeField] private string sceneToLoad;
+    [SerializeField] private string sceneTransitionName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>())
+        {
+            SceneManager.LoadScene(sceneToLoad);
+            SceneManagement.Instance.SetTransitionName(sceneTransitionName);
+        }
+    }
+}
